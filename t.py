df1befe5ad97a5aec144dca99bb3e52a58e788f7
Steve Losh <steve@stevelosh.com>
2020-04-11 11:33:49 -0400

    Whitespace

--- t.py
+++ t.py
@@ -177,7 +177,6 @@ class TaskDict(object):
         task_id = _hash(text)
         self.tasks[task_id] = {'id': task_id, 'text': text}
 
-
     def edit_task(self, prefix, text):
         """Edit the task with the given prefix.
 
